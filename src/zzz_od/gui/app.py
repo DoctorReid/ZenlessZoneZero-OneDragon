99ae195a46d417aaeee103fabe37ab21a52256cc
DoctorReid <momojie_dyh@163.com>
2024-07-04 17:44:42 +0800

    #1 增加app主页

--- src/zzz_od/gui/app.py
+++ src/zzz_od/gui/app.py
@@ -3,14 +3,14 @@ import sys
 
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
-from qfluentwidgets import FluentIcon
+from qfluentwidgets import NavigationItemPosition
 
 from one_dragon.envs.project_config import project_config
 from one_dragon.gui.view.base_window import BaseFluentWindow
 from one_dragon.gui.view.code_interface import CodeInterface
-from one_dragon.gui.view.install_interface import InstallerInterface
 from one_dragon.utils import os_utils
 from one_dragon.utils.i18_utils import gt
+from zzz_od.gui.view.app_setting_interface import AppSettingInterface
 from zzz_od.gui.view.home_interface import HomeInterface
 
 
@@ -23,11 +23,17 @@ class InstallerWindow(BaseFluentWindow):
         self.project_config = project_config
         self.home_interface = HomeInterface(self)
 
+        self.code_interface = CodeInterface(self)
+        self.setting_interface = AppSettingInterface(self)
+
         self.init_navigation()
         self.init_window()
 
     def init_navigation(self):
-        self.addSubInterface(self.home_interface, FluentIcon.CLOUD_DOWNLOAD, '主页')
+        self.addSubInterface(self.home_interface, self.home_interface.nav_icon, self.home_interface.nav_text)
+
+        self.addSubInterface(self.code_interface, self.code_interface.nav_icon, self.code_interface.nav_text, position=NavigationItemPosition.BOTTOM)
+        self.addSubInterface(self.setting_interface, self.setting_interface.nav_icon, self.setting_interface.nav_text, position=NavigationItemPosition.BOTTOM)
 
     def init_window(self):
         title = f"{gt(self.project_config.project_name, 'ui')}"
--- src/zzz_od/gui/view/home_interface.py
+++ src/zzz_od/gui/view/home_interface.py
@@ -2,11 +2,13 @@ import os
 
 from PySide6.QtCore import Qt, QRectF
 from PySide6.QtGui import QPixmap, QPainter, QColor, QBrush, QPainterPath, QLinearGradient
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QSpacerItem, QSizePolicy
-from qfluentwidgets import ScrollArea, isDarkTheme, FluentIcon
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QSpacerItem, QSizePolicy
+from qfluentwidgets import isDarkTheme, FluentIcon
 
+from one_dragon.gui.component.interface.vertical_scroll_interface import VerticalScrollInterface
 from one_dragon.gui.component.link_card import LinkCardView
 from one_dragon.utils import os_utils
+from one_dragon.utils.i18_utils import gt
 
 
 class BannerWidget(QWidget):
@@ -17,38 +19,33 @@ class BannerWidget(QWidget):
         self.setFixedHeight(336)
 
         self.vBoxLayout = QVBoxLayout(self)
-        self.galleryLabel = QLabel('Fluent Gallery', self)
         self.banner = QPixmap(os.path.join(os_utils.get_path_under_work_dir('assets', 'ui'), '1.png'))
         self.linkCardView = LinkCardView(self)
 
-        self.galleryLabel.setObjectName('galleryLabel')
-
         self.vBoxLayout.setSpacing(0)
         self.vBoxLayout.setContentsMargins(0, 20, 0, 0)
-        self.vBoxLayout.addWidget(self.galleryLabel)
-        self.vBoxLayout.addWidget(self.linkCardView, 1, Qt.AlignBottom)
-        self.vBoxLayout.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        self.vBoxLayout.addWidget(self.linkCardView, 1, Qt.AlignmentFlag.AlignBottom)
+        self.vBoxLayout.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
 
-        spacer = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Fixed)
+        spacer = QSpacerItem(20, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
         self.vBoxLayout.addItem(spacer)
 
         self.linkCardView.addCard(
             FluentIcon.GITHUB,
-            self.tr('GitHub repo'),
+            gt('主页', 'ui'),
             self.tr(
-                'The latest fluent design controls and styles for your applications.'),
-            "test"
+                '喜欢本项目请前往主页点Star'),
+            "https://github.com/DoctorReid/ZenlessZoneZero-OneDragon"
         )
 
     def paintEvent(self, e):
         super().paintEvent(e)
         painter = QPainter(self)
-        painter.setRenderHints(
-            QPainter.SmoothPixmapTransform | QPainter.Antialiasing)
-        painter.setPen(Qt.NoPen)
+        painter.setRenderHints(QPainter.RenderHint.SmoothPixmapTransform | QPainter.RenderHint.Antialiasing)
+        painter.setPen(Qt.PenStyle.NoPen)
 
         path = QPainterPath()
-        path.setFillRule(Qt.WindingFill)
+        path.setFillRule(Qt.FillRule.WindingFill)
         w, h = self.width(), self.height()
         path.addRoundedRect(QRectF(0, 0, w, h), 10, 10)
         path.addRect(QRectF(0, h-50, 50, 50))
@@ -71,31 +68,24 @@ class BannerWidget(QWidget):
 
         # draw banner image
         pixmap = self.banner.scaled(
-            self.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+            self.size(), Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation)
         painter.fillPath(path, QBrush(pixmap))
 
 
-class HomeInterface(ScrollArea):
+class HomeInterface(VerticalScrollInterface):
     """ Home interface """
 
     def __init__(self, parent=None):
-        super().__init__(parent=parent)
-        self.banner = BannerWidget(self)
-        self.view = QWidget(self)
-        self.vBoxLayout = QVBoxLayout(self.view)
+        content_widget = QWidget()
+        content_layout = QVBoxLayout(content_widget)
 
-        self.__initWidget()
+        content_layout.setContentsMargins(0, 0, 0, 36)
+        content_layout.setSpacing(40)
+        content_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
-    def __initWidget(self):
-        self.view.setObjectName('view')
-        self.setObjectName('homeInterface')
-        # StyleSheet.HOME_INTERFACE.apply(self)
+        banner = BannerWidget()
+        content_layout.addWidget(banner)
 
-        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.setWidget(self.view)
-        self.setWidgetResizable(True)
-
-        self.vBoxLayout.setContentsMargins(0, 0, 0, 36)
-        self.vBoxLayout.setSpacing(40)
-        self.vBoxLayout.addWidget(self.banner)
-        self.vBoxLayout.setAlignment(Qt.AlignTop)
+        VerticalScrollInterface.__init__(self, parent=parent,
+                                         content_widget=content_widget, object_name='home_interface',
+                                         nav_text_cn='主页', nav_icon=FluentIcon.HOME)
