0ffcdda645c35693deebdf01415bdcac77a7fd8f
依币 <73031747+yibiya@users.noreply.github.com>
2024-07-19 21:05:10 +0800

    Update app.bat

--- app.bat
+++ app.bat
@@ -1,4 +1,26 @@
 @echo off
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+
+goto UACPrompt
+
+) else ( goto gotAdmin )
+
+:UACPrompt
+
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+"%temp%\getadmin.vbs"
+
+exit /B
+
+:gotAdmin
+
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
 chcp 65001
 
 call %~dp0/env.bat
