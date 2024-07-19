f6568bf8f5b8bcd9198ce57d5e268d9edc680782
依币 <73031747+yibiya@users.noreply.github.com>
2024-07-19 21:03:57 +0800

    Update app.bat

--- app.bat
+++ app.bat
@@ -21,3 +21,16 @@ exit /B
 :gotAdmin
 
 if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+chcp 65001
+
+call %~dp0/env.bat
+
+set PYTHONPATH=%~dp0/src
+
+echo 启动中...大约需要10+秒
+
+start "zzz-od-app" %PYTHON% %~dp0/src/zzz_od/gui/app.py > %~dp0/.log/bat.log
+
+timeout /t 10
+
+exit 0
