03c23bd72e89bacf3c416758fdd3d26e70a53455
DoctorReid <momojie_dyh@163.com>
2024-08-02 11:20:17 +0800

    #32 邮件应用改用标注

--- src/zzz_od/application/email/email_app.py
+++ src/zzz_od/application/email/email_app.py
@@ -1,5 +1,6 @@
+from one_dragon.base.operation.operation_edge import node_from
+from one_dragon.base.operation.operation_node import operation_node
 from one_dragon.base.operation.operation_round_result import OperationRoundResult
-from one_dragon.base.operation.operation_node import OperationNode
 from one_dragon.utils.i18_utils import gt
 from zzz_od.application.zzz_application import ZApplication
 from zzz_od.context.zzz_context import ZContext
@@ -19,28 +20,6 @@ class EmailApp(ZApplication):
             run_record=ctx.email_run_record
         )
 
-    def add_edges_and_nodes(self) -> None:
-        """
-        初始化前 添加边和节点 由子类实行
-        :return:
-        """
-        open_menu = OperationNode('打开菜单', op=OpenMenu(self.ctx))
-        click_more = OperationNode('点击更多', self.click_more)
-        self.add_edge(open_menu, click_more)
-
-        click_email = OperationNode('点击邮件', self.click_email)  # TODO 是否需要红点检测
-        self.add_edge(click_more, click_email)
-
-        click_get_all = OperationNode('全部领取', self.click_get_all)
-        self.add_edge(click_email, click_get_all)
-
-        click_confirm = OperationNode('确认', self.click_confirm)
-        self.add_edge(click_get_all, click_confirm)
-
-        back_to_menu = OperationNode('返回菜单', self.back_to_menu)
-        self.add_edge(click_confirm, back_to_menu)
-        self.add_edge(click_confirm, back_to_menu, success=False)  # 没有东西领取的话 就不会有确认按钮
-
     def handle_init(self) -> None:
         """
         执行前的初始化 由子类实现
@@ -48,6 +27,13 @@ class EmailApp(ZApplication):
         """
         pass
 
+    @operation_node(name='打开菜单', is_start_node=True)
+    def open_menu(self) -> OperationRoundResult:
+        op = OpenMenu(self.ctx)
+        return self.round_by_op(op.execute())
+
+    @node_from(from_name='打开菜单')
+    @operation_node(name='点击更多')
     def click_more(self) -> OperationRoundResult:
         """
         点击更多
@@ -56,6 +42,8 @@ class EmailApp(ZApplication):
         return self.round_by_find_and_click_area(screen, '菜单', '更多',
                                                  success_wait=1, retry_wait_round=1)
 
+    @node_from(from_name='点击更多')
+    @operation_node(name='')
     def click_email(self) -> OperationRoundResult:
         """
         在菜单页面 点击邮件
@@ -65,15 +53,20 @@ class EmailApp(ZApplication):
         return self.round_by_find_and_click_area(screen, '菜单', '邮件',
                                                  success_wait=1, retry_wait_round=1)
 
+    @node_from(from_name='点击邮件')
+    @operation_node(name='全部领取')
     def click_get_all(self) -> OperationRoundResult:
         """
         邮件画面 点击全部领取
+        就算时灰色的也能识别到
         :return:
         """
         screen = self.screenshot()
         return self.round_by_find_and_click_area(screen, '邮件', '全部领取',
                                                  success_wait=1, retry_wait_round=1)
 
+    @node_from(from_name='全部领取')
+    @operation_node(name='确认')
     def click_confirm(self) -> OperationRoundResult:
         """
         邮件画面 领取后点击确认
@@ -83,9 +76,14 @@ class EmailApp(ZApplication):
         return self.round_by_find_and_click_area(screen, '邮件', '确认',
                                                  success_wait=1, retry_wait_round=1)
 
+    @node_from(from_name='确认')  # 确认之后返回
+    @node_from(from_name='确认', success=False)  # 没有确认 其实就是没有东西能领取 也返回
+    @node_from(from_name='全部领取', success=False)  # 没找到全部领取的话 也返回
+    @operation_node(name='返回菜单')
     def back_to_menu(self) -> OperationRoundResult:
         """
         返回菜单
+        领取后的确认按钮可以不按 直接点击外层也可以返回
         :return:
         """
         screen = self.screenshot()
