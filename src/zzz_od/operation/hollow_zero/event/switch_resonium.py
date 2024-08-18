9ac4c12d934098bca5efe1dab6f7d9263113e41b
DoctorReid <momojie_dyh@163.com>
2024-08-18 16:28:48 +0800

    #58 补充事件

--- src/zzz_od/operation/hollow_zero/event/switch_resonium.py
+++ src/zzz_od/operation/hollow_zero/event/switch_resonium.py
@@ -1,15 +1,21 @@
 import time
 
+from typing import List
+
+from cv2.typing import MatLike
+
+from one_dragon.base.matcher.match_result import MatchResultList, MatchResult
 from one_dragon.base.operation.operation_edge import node_from
 from one_dragon.base.operation.operation_node import operation_node
 from one_dragon.base.operation.operation_round_result import OperationRoundResult
+from one_dragon.utils import cv2_utils, str_utils
 from one_dragon.utils.i18_utils import gt
 from zzz_od.context.zzz_context import ZContext
 from zzz_od.operation.hollow_zero.event import resonium_utils
 from zzz_od.operation.zzz_operation import ZOperation
 
 
-class DropResonium(ZOperation):
+class SwitchResonium(ZOperation):
 
     def __init__(self, ctx: ZContext):
         """
@@ -18,15 +24,14 @@ class DropResonium(ZOperation):
         """
         ZOperation.__init__(
             self, ctx,
-            node_max_retry_times=5,
-            op_name=gt('丢弃鸣徽')
+            op_name=gt('交换鸣徽')
         )
 
     @operation_node(name='选择', is_start_node=True)
     def choose_one(self) -> OperationRoundResult:
         screen = self.screenshot()
 
-        item_list = resonium_utils.get_to_choose_list(self.ctx, screen, '丢弃')
+        item_list = resonium_utils.get_to_choose_list(self.ctx, screen, '交换')
         if len(item_list) == 0:
             return self.round_retry(status='识别不到选项', wait=0.5)
 
@@ -35,7 +40,7 @@ class DropResonium(ZOperation):
         if len(idx_list) == 0:
             return self.round_retry(status='优先级无返回', wait=0.5)
 
-        mr = item_list[idx_list[-1]]  # 丢弃优先级最低的
+        mr = item_list[idx_list[0]]
         self.ctx.controller.click(mr.center)
         time.sleep(0.1)
         return self.round_by_click_area('零号空洞-事件', '空白', success_wait=0.9)
@@ -45,5 +50,23 @@ class DropResonium(ZOperation):
     def choose_default(self):
         screen = self.screenshot()
         area = self.ctx.screen_loader.get_area('零号空洞-事件', '底部-选择列表')
-        return self.round_by_ocr_and_click(screen, '丢弃', area=area,
-                                           success_wait=1, retry_wait=1)
+        return self.round_by_ocr_and_click(screen, '交换', area=area,
+                                           success_wait=1, retry_wait=1,
+                                           color_range=[(240, 240, 240), (255, 255, 255)])
+
+
+
+
+
+
+def __debug():
+    ctx = ZContext()
+    ctx.init_by_config()
+    ctx.start_running()
+    ctx.ocr.init_model()
+    op = ChooseResonium(ctx)
+    op.execute()
+
+
+if __name__ == '__main__':
+    __debug()
