81d5aeb70fcc662a5294c473749748f2f28e4cc3
DoctorReid <momojie_dyh@163.com>
2024-08-19 13:13:44 +0800

    #58 更新可用模型

--- src/one_dragon/utils/yolo_config_utils.py
+++ src/one_dragon/utils/yolo_config_utils.py
@@ -2,7 +2,6 @@ import os
 
 from typing import List
 
-from one_dragon.base.config.config_item import ConfigItem
 from one_dragon.utils import os_utils
 
 
@@ -21,26 +20,6 @@ def get_available_models(category: str) -> List[str]:
             result_list.append(sub_dir_name)
 
     return result_list
-
-
-def get_flash_classifier_opts() -> List[ConfigItem]:
-    """
-    获取闪光模型的选项
-    :return:
-    """
-    models_list = get_available_models('flash_classifier') + [ 'yolov8n-640-flash-0718' ]
-
-    return [ConfigItem(i) for i in models_list]
-
-
-def get_hollow_zero_event_opts() -> List[ConfigItem]:
-    """
-    获取空洞模型的选项
-    :return:
-    """
-    models_list = get_available_models('hollow_zero_event') + [ 'yolov8s-640-hollow-zero-event-0818' ]
-
-    return [ConfigItem(i) for i in models_list]
 
 
 def is_model_existed(category: str, model_name: str) -> bool:
--- src/zzz_od/config/yolo_config.py
+++ src/zzz_od/config/yolo_config.py
@@ -1,4 +1,8 @@
+from typing import List
+
+from one_dragon.base.config.config_item import ConfigItem
 from one_dragon.base.config.yaml_config import YamlConfig
+from one_dragon.utils import yolo_config_utils
 
 ZZZ_MODEL_DOWNLOAD_URL = 'https://github.com/DoctorReid/OneDragon-YOLO/releases/download/zzz_model'
 
@@ -24,3 +28,22 @@ class YoloConfig(YamlConfig):
     def hollow_zero_event(self, new_value: str) -> None:
         self.update('hollow_zero_event', new_value)
 
+
+def get_flash_classifier_opts() -> List[ConfigItem]:
+    """
+    获取闪光模型的选项
+    :return:
+    """
+    models_list = yolo_config_utils.get_available_models('flash_classifier') + [ 'yolov8n-640-flash-0718' ]
+
+    return [ConfigItem(i) for i in models_list]
+
+
+def get_hollow_zero_event_opts() -> List[ConfigItem]:
+    """
+    获取空洞模型的选项
+    :return:
+    """
+    models_list = yolo_config_utils.get_available_models('hollow_zero_event') + [ 'yolov8s-640-hollow-zero-event-0819' ]
+
+    return [ConfigItem(i) for i in models_list]
