493e41f30e3ac3478a95bc43004b8662321d7b73
DoctorReid <doctorreid2024@outlook.com>
2024-09-01 10:30:07 +0800

    #114 简单处理好感度事件

--- src/zzz_od/operation/back_to_normal_world.py
+++ src/zzz_od/operation/back_to_normal_world.py
@@ -1,7 +1,11 @@
+from cv2.typing import MatLike
+
 from one_dragon.base.operation.operation_node import OperationNode
 from one_dragon.base.operation.operation_round_result import OperationRoundResult
+from one_dragon.utils import cv2_utils, str_utils
 from one_dragon.utils.i18_utils import gt
 from zzz_od.context.zzz_context import ZContext
+from zzz_od.game_data.agent import AgentEnum
 from zzz_od.operation.zzz_operation import ZOperation
 
 
@@ -13,7 +17,7 @@ class BackToNormalWorld(ZOperation):
         :param ctx:
         """
         ZOperation.__init__(self, ctx,
-                            node_max_retry_times=5,
+                            node_max_retry_times=60,
                             op_name=gt('返回大世界', 'ui')
                             )
 
@@ -41,14 +45,58 @@ class BackToNormalWorld(ZOperation):
         if result.is_success:
             return self.round_success()
 
+        # 这是领取完活跃度奖励的情况
         result = self.round_by_find_area(screen, '快捷手册', 'TAB-挑战')
-
         if result.is_success:
             self.click_area('快捷手册', '退出')
             return self.round_retry(wait=1)
+
+        # 判断是否有好感度事件
+        if self._check_agent_dialog(screen):
+            return self._handle_agent_dialog(screen)
 
         click_back = self.click_area('菜单', '返回')
         if click_back:
             return self.round_retry(wait_round_time=1)
         else:
             return self.round_fail()
+
+    def _check_agent_dialog(self, screen: MatLike) -> bool:
+        """
+        识别是否有代理人好感度对话
+        """
+        area = self.ctx.screen_loader.get_area('大世界', '好感度标题')
+        part = cv2_utils.crop_image_only(screen, area.rect)
+        ocr_result_map = self.ctx.ocr.run_ocr(part)
+        ocr_result_list = [i for i in ocr_result_map.keys()]
+        agent_name_list = [gt(i.value.agent_name) for i in AgentEnum]
+        idx1, idx2 = str_utils.find_most_similar(ocr_result_list, agent_name_list)
+        return idx1 is not None and idx2 is not None
+
+    def _handle_agent_dialog(self, screen: MatLike) -> OperationRoundResult:
+        """
+        处理代理人好感度对话
+        """
+        area = self.ctx.screen_loader.get_area('大世界', '好感度选项')
+        part = cv2_utils.crop_image_only(screen, area.rect)
+        ocr_result_map = self.ctx.ocr.run_ocr(part)
+        if len(ocr_result_map) > 0:
+            for ocr_result, mrl in ocr_result_map.items():
+                self.ctx.controller.click(mrl.max.center + area.left_top)
+                return self.round_wait(ocr_result, wait=1)
+        else:
+            self.click_area('菜单', '返回')
+            return self.round_wait('对话无选项', wait=1)
+
+
+def __debug_op():
+    ctx = ZContext()
+    ctx.init_by_config()
+    ctx.ocr.init_model()
+    op = BackToNormalWorld(ctx)
+    ctx.start_running()
+    op.execute()
+
+
+if __name__ == '__main__':
+    __debug_op()
