7240cf4f9edd79d537b69bdec2ad689fff331efc
yokuminto <yokuminto@qq.com>
2024-09-08 00:52:23 +0800

    调整app.bat、调整app.py

--- app.bat
+++ app.bat
@@ -1,13 +1,15 @@
 @echo off
-chcp 65001 >nul 2>&1
+chcp 65001 2>&1
 
 rem 检查是否以管理员权限运行
-net session >nul 2>&1
+net session 2>&1
 if %errorlevel% neq 0 (
     echo -------------------------------
-    echo 需要请求管理员权限!
+    echo 尝试获取管理员权限中...
     echo -------------------------------
-    PowerShell -Command "Start-Process '%~dpnx0'  -WindowStyle Hidden -Verb RunAs"
+    rem 增加延迟时间，如遇无限循环，则可在此终止程序运行
+    timeout /t 2
+    PowerShell -Command "Start-Process '%~dpnx0' -Verb RunAs"
     exit /b
 )
 
@@ -15,27 +17,31 @@ echo -------------------------------
 echo 正在以管理员权限运行...
 echo -------------------------------
 
-
 set "MAINPATH=zzz_od\gui\app.py"
+set "ENV_DIR=%~dp0.env"
 
 REM 调用环境配置脚本
 call "%~dp0env.bat"
 set "PYTHONPATH=%~dp0src"
 set "APPPATH=%PYTHONPATH%\%MAINPATH%"
-echo PYTHON=%PYTHON%
-echo PYTHONPATH=%PYTHONPATH%
-echo APPPATH=%APPPATH%
+set "PYTHONUSERBASE=%~dp0.env"
+
+REM 打印信息
+echo [PASS] PYTHON：%PYTHON%
+echo [PASS] PYTHONPATH：%PYTHONPATH%
+echo [PASS] APPPATH：%APPPATH%
+echo [PASS] PYTHONUSERBASE：%PYTHONUSERBASE%
 
 REM 使用 PowerShell 检查路径中是否有中文字符
 powershell -command "if ('%~dp0' -match '[\u4e00-\u9fff]') { exit 1 } else { exit 0 }"
 if %errorlevel% equ 1 (
-    echo 警告：当前路径包含中文字符
+    echo [WARN] 当前路径包含中文字符
 )
 
 REM 检查路径中是否有空格
 set "path_check=%~dp0"
 if "%path_check%" neq "%path_check: =%" (
-    echo 警告：路径中包含空格
+    echo [WARN] 路径中包含空格
 )
 
 REM 获取当前日期并格式化为 YYYYMMDD
@@ -61,54 +67,58 @@ REM 生成日志目录和文件名，格式为 YYYYMMDD 和 HH.MM.SS
 set log_dir=%~dp0.log\%year%%month%%day%
 set timestamp=%hour%.%minute%.%second%
 set "BAT_LOG=%log_dir%\bat_%timestamp%.log"
+set "PYTHON_LOG=%log_dir%\python_%timestamp%.log"
 
 REM 检查并创建日志目录
 if not exist "%log_dir%" (
-    echo 日志目录不存在，正在创建...
+    echo [WARN] 日志目录不存在，正在创建...
     mkdir "%log_dir%"
     if %errorlevel% neq 0 (
-        echo 创建日志目录失败。
+        echo [WARN] 创建日志目录失败。
         pause
         exit /b 1
     )
-    echo 日志目录创建成功。
+    echo [PASS] 日志目录创建成功。
 )
 
 REM 删除所有以 'bat_' 开头且以 '.log' 结尾的文件
 for /r "%log_dir%" %%F in (bat_*.log) do (
     del "%%F"
-    echo 删除旧日志文件: %%F
+    echo [INFO] 删除旧日志文件: %%F
 )
 
 REM 检查 Python 可执行文件路径
 if not exist "%PYTHON%" (
-    echo "未配置Python.exe"
+    echo [WARN] 未配置Python.exe
     pause
     exit /b 1
 )
 
 REM 检查 PythonPath 目录
 if not exist "%PYTHONPATH%" (
-    echo "PYTHONPATH 未设置"
+    echo [WARN] PYTHONPATH 未设置
+    pause
+    exit /b 1
+)
+
+REM 检查 PythonUserBase 目录
+if not exist "%PYTHONUSERBASE%" (
+    echo [WARN] PYTHONUSERBASE 未设置
     pause
     exit /b 1
 )
 
 REM 检查应用程序脚本路径
 if not exist "%APPPATH%" (
-    echo "PYTHONPATH 设置错误 无法找到 %APPPATH%"
+    echo [WARN] PYTHONPATH 设置错误 无法找到 %APPPATH%
     pause
     exit /b 1
 )
 
-echo "启动中...大约需要10+秒"
-"%PYTHON%" "%APPPATH%" >> "%BAT_LOG%" 2>&1
-if %errorlevel% neq 0 (
-    echo "运行出错 请将错误信息反馈 %BAT_LOG%"
-    pause
-    exit /b 1
-)
+echo 启动中...大约需要5+秒
+powershell -Command "& {Start-Process '%PYTHON%' -ArgumentList '%APPPATH%' -NoNewWindow -RedirectStandardOutput '%PYTHON_LOG%' -PassThru}"
 
-timeout /t 10
+REM 等待一段时间后退出
+timeout /t 5
 
 exit 0
