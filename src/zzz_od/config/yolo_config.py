7cdf53e48b6ea78d1e85cbe397041d6ae9b09a06
DoctorReid <doctorreid2024@outlook.com>
2024-10-30 14:13:19 +0800

    #330 #339 更新空洞模型

--- src/zzz_od/config/yolo_config.py
+++ src/zzz_od/config/yolo_config.py
@@ -7,7 +7,7 @@ from one_dragon.utils import yolo_config_utils
 
 ZZZ_MODEL_DOWNLOAD_URL = 'https://github.com/DoctorReid/OneDragon-YOLO/releases/download/zzz_model'
 _DEFAULT_FLASH_CLASSIFIER = 'yolov8n-640-flash-0718'
-_DEFAULT_HOLLOW_ZERO_EVENT = 'yolov8s-736-hollow-zero-event-0922'
+_DEFAULT_HOLLOW_ZERO_EVENT = 'yolov8s-736-hollow-zero-event-1027'
 
 
 class YoloConfig(YamlConfig):
