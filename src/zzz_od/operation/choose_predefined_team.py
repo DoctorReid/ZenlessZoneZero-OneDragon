3031fdc9b7dcc83c60d74a5c83237e9abf686a39
DoctorReid <doctorreid2024@outlook.com>
2024-11-24 12:53:49 +0800

    #435 修复选择编队6以上的队伍

--- src/zzz_od/operation/choose_predefined_team.py
+++ src/zzz_od/operation/choose_predefined_team.py
@@ -24,9 +24,7 @@ class ChoosePredefinedTeam(ZOperation):
         ZOperation.__init__(self, ctx, op_name=gt('选择预备编队', 'ui'))
 
         self.target_team_idx_list: List[int] = target_team_idx_list
-
-    def handle_init(self):
-        pass
+        self.choose_fail_times: int = 0   # 选择失败的次数
 
     @operation_node(name='画面识别', node_max_retry_times=10, is_start_node=True)
     def check_screen(self) -> OperationRoundResult:
@@ -46,6 +44,7 @@ class ChoosePredefinedTeam(ZOperation):
                                                  success_wait=1, retry_wait=1)
 
     @node_from(from_name='点击预备编队')
+    @node_from(from_name='选择编队失败')
     @operation_node(name='选择编队')
     def choose_team(self) -> OperationRoundResult:
         screen = self.screenshot()
@@ -69,11 +68,11 @@ class ChoosePredefinedTeam(ZOperation):
             best_match = difflib.get_close_matches(target_team_name, target_list, n=1)
 
             if best_match is None or len(best_match) == 0:
-                return self.round_retry(wait=1)
+                return self.round_retry(wait=0.5)
 
             ocr_result: MatchResultList = ocr_map.get(best_match[0], None)
             if ocr_result is None or ocr_result.max is None:
-                return self.round_retry(wait=1)
+                return self.round_retry(wait=0.5)
 
             to_click = ocr_result.max.center + Point(200, 0)
             self.ctx.controller.click(to_click)
@@ -81,6 +80,18 @@ class ChoosePredefinedTeam(ZOperation):
             time.sleep(1)
 
         return self.round_wait()
+
+    @node_from(from_name='选择编队', success=False)
+    @operation_node(name='选择编队失败')
+    def choose_team_fail(self) -> OperationRoundResult:
+        self.choose_fail_times += 1
+        if self.choose_fail_times >= 2:
+            return self.round_fail('选择配队失败')
+
+        drag_start = Point(self.ctx.controller.standard_width // 2, self.ctx.controller.standard_height // 2)
+        drag_end = drag_start + Point(0, -500)
+        self.ctx.controller.drag_to(start=drag_start, end=drag_end)
+        return self.round_success(wait=1)
 
     @node_from(from_name='选择编队')
     @operation_node(name='选择编队确认')
