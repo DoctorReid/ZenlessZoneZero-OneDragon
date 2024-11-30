aa5935e8741050e83956d3420cce85499c4a6a6c
DoctorReid <doctorreid2024@outlook.com>
2024-11-30 23:54:21 +0800

    #452 空洞 更新模型

--- src/zzz_od/application/shiyu_defense/shiyu_defense_run_record.py
+++ src/zzz_od/application/shiyu_defense/shiyu_defense_run_record.py
@@ -25,8 +25,8 @@ class ShiyuDefenseRunRecord(AppRunRecord):
         )
 
         self.critical_nodes: list[CriticalNode] = [
-            CriticalNode(7, '20241101', '20241115'),
             CriticalNode(7, '20241116', '20241131'),
+            CriticalNode(7, '20241201', '20241215'),
         ]
 
     @property
