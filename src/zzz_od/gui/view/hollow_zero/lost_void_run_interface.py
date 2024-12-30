c13bc3984c03623696688076fa8a92b44ba064d8
DoctorReid <doctorreid2024@outlook.com>
2024-12-30 19:30:09 +0800

    #529 迷失之地 补充选项

--- src/zzz_od/gui/view/hollow_zero/lost_void_run_interface.py
+++ src/zzz_od/gui/view/hollow_zero/lost_void_run_interface.py
@@ -50,7 +50,7 @@ class LostVoidRunInterface(AppRunInterface):
         left_widget.setLayout(left_layout)
 
         self.help_opt = HyperlinkCard(icon=FluentIcon.HELP, title='使用说明', text='前往',
-                                      url='https://one-dragon.org/zzz/zh/docs/feat_hollow_zero.html')
+                                      url='https://one-dragon.org/zzz/zh/docs/feat_lost_void.html')
         self.help_opt.setContent('先看说明 再使用与提问')
         left_layout.addWidget(self.help_opt)
 
