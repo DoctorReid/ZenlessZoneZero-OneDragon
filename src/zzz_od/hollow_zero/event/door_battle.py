f0a4cc31edd3b0d81cdaf12a4aeb9af061dc8554
DoctorReid <doctorreid2024@outlook.com>
2025-02-05 09:25:27 +0800

    #737 门扉禁闭-善战 兼容识别错误情况

--- src/zzz_od/hollow_zero/event/door_battle.py
+++ src/zzz_od/hollow_zero/event/door_battle.py
@@ -25,6 +25,11 @@ class DoorBattle(ZOperation):
             EventOcrResultHandler(HollowZeroSpecialEvent.DOOR_BATTLE_ENTRY.value.event_name, click_wait=3),
         ]
 
+        for e in self.ctx.hollow.data_service.normal_events:
+            if e.entry_name == '门扉禁闭-侵蚀':  # 兼容识别错误的情况
+                for opt in e.options:
+                    self._handlers.append(EventOcrResultHandler(opt.option_name))
+
     @operation_node(name='画面识别', is_start_node=True)
     def check_screen(self) -> OperationRoundResult:
         screen = self.screenshot()
