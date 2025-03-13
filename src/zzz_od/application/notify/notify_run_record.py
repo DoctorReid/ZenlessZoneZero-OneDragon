12b6c110724e96bf983fcc615a035764d1e217a5
HaneRo <piumarugiada@hotmail.com>
2025-03-13 21:48:08 +0800

    通知每次都运行

--- src/zzz_od/application/notify/notify_run_record.py
+++ src/zzz_od/application/notify/notify_run_record.py
@@ -11,5 +11,13 @@ class NotifyRunRecord(AppRunRecord):
         AppRunRecord.__init__(self, 'notify', instance_idx=instance_idx,
                               game_refresh_hour_offset=game_refresh_hour_offset)
         
+    def check_and_update_status(self):
+        self.reset_record()
 
-
+    @property
+    def run_status_under_now(self):
+        """
+        永远运行
+        :return:
+        """
+        return AppRunRecord.STATUS_WAIT
