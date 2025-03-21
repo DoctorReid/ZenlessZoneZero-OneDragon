9a9362f8efa6e80bde89f2ca085fe8abce453cf8
Qian23333 <hzq2312618734@outlook.com>
2025-03-21 23:48:21 +0800

    增加文字提示

--- src/one_dragon_qt/view/code_interface.py
+++ src/one_dragon_qt/view/code_interface.py
@@ -66,7 +66,7 @@ class CodeInterface(VerticalScrollInterface):
         v_layout.addWidget(self.code_card)
 
         self.branch_opt = ComboBoxSettingCard(
-            icon=FluentIcon.SYNC, title='分支选择', content='选择代码分支',
+            icon=FluentIcon.SYNC, title='分支选择', content='选择代码分支，选择后请点击同步最新代码',
             options_enum=GitBranchEnum,
         )
         v_layout.addWidget(self.branch_opt)
