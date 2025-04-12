5823a91b98eee8d8aa643048be65aa50d14129e5
Qian23333 <119576779+Qian23333@users.noreply.github.com>
2025-04-13 05:46:33 +0800

    *可能会导致性能问题* 匹配所有皮肤开关

--- src/zzz_od/context/hollow_context.py
+++ src/zzz_od/context/hollow_context.py
@@ -50,9 +50,10 @@ class HollowContext:
         for agent in possible_agents:
             if agent is None:
                 continue
-            mrl = self.ctx.tm.match_template(img, 'hollow', prefix + agent.template_id, threshold=0.8)
-            if mrl.max is not None:
-                return agent
+            for template_id in agent.template_id_list:
+                mrl = self.ctx.tm.match_template(img, 'hollow', prefix + template_id, threshold=0.8)
+                if mrl.max is not None:
+                    return agent
 
         return None
 
