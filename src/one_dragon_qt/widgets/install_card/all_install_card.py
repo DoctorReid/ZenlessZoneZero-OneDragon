f93345179e55b5403a3f65105c9849d2a9786e4e
Qian23333 <hzq2312618734@outlook.com>
2025-05-24 23:20:20 +0800

    feat: 添加一键安装布局

--- src/one_dragon_qt/widgets/install_card/all_install_card.py
+++ src/one_dragon_qt/widgets/install_card/all_install_card.py
@@ -43,6 +43,9 @@ class AllInstallCard(BaseInstallCard):
         log.info('一键安装 开始')
         self.update_display(None, gt('安装中', 'ui'))
         self.installing_idx = 0
+        self._progress_callback = progress_callback  # 保存回调
+        if progress_callback is not None:
+            progress_callback(self.installing_idx / len(self.install_cards), gt('安装中', 'ui'))
         self.install_cards[self.installing_idx].start_progress()
         return True, '成功'
 
@@ -58,14 +61,19 @@ class AllInstallCard(BaseInstallCard):
             self.update_display(None,
                                 f"{gt('安装失败', 'ui')} {self.install_cards[self.installing_idx].title}")
             self.installing_idx = -1
+            self._progress_callback = None
         else:
             log.info('一键安装 开始下一个')
             self.installing_idx += 1
+            # 每次子卡片完成时调用回调
+            if hasattr(self, '_progress_callback') and self._progress_callback is not None:
+                self._progress_callback(self.installing_idx / len(self.install_cards), gt('安装中', 'ui'))
             if self.installing_idx < len(self.install_cards):
                 self.install_cards[self.installing_idx].start_progress()
             else:
                 self.update_display(FluentIcon.INFO.icon(color=FluentThemeColor.DEFAULT_BLUE.value),
                                     gt('安装成功', 'ui'))
+                self._progress_callback = None
 
     def _on_run_clicked(self) -> None:
         """
