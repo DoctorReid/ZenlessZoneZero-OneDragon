26e30083f26c68feb3cec9b98e38fc1161db77cd
Paper-white <841968078@qq.com>
2025-06-11 05:05:45 +0800

    feat: 适配仪玄皮肤

--- src/zzz_od/config/agent_outfit_config.py
+++ src/zzz_od/config/agent_outfit_config.py
@@ -56,6 +56,18 @@ class AgentOutfitConfig(YamlConfig):
     def astra_yao(self, value: str) -> None:
         self.update('astra_yao', value)
 
+    @property
+    def yixuan_outfit_list(self) -> str:
+        return self.get('yixuan_outfit_list', [AgentOutfitYiXuan.DEFAULT.value.value,
+                                                AgentOutfitYiXuan.TRAILS_OF_INK.value.value])
+
+    @property
+    def yixuan(self) -> str:
+        return self.get('yixuan', AgentOutfitYiXuan.DEFAULT.value.value)
+
+    @yixuan.setter
+    def yixuan(self, value: str) -> None:
+        self.update('yixuan', value)
 
 class AgentOutfitNicole(Enum):
     """
@@ -79,3 +91,10 @@ class AgentOutfitAstraYao(Enum):
     """
     DEFAULT = ConfigItem('默认', 'astra_yao')
     CHANDELIER = ConfigItem('水晶灯下', 'astra_yao_chandelier')
+
+class AgentOutfitYiXuan(Enum):
+    """
+    仪玄皮肤
+    """
+    DEFAULT = ConfigItem('默认', 'yixuan')
+    TRAILS_OF_INK = ConfigItem('墨形影踪', 'yixuan_trails_of_ink')
